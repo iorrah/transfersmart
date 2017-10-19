6a5f1aa29a6a20b80c4df8b9699acb863d93d6c2
Iorrah Gonzalez <iorrah.gonzalez@gmail.com>
2017-10-19 14:44:09 +0200

    Footer: implementing initial structure

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -4,7 +4,9 @@ import './Footer.css';
 class Footer extends React.Component {
   render() {
     return (
-      <footer></footer>
+      <footer>
+        © 2017 Copyright TransferSmart
+      </footer>
     );
   }
 }
