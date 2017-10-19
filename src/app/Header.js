492ad2264364dbe7b75645c6c5768d8f4fa5c2db
Iorrah Gonzalez <iorrah.gonzalez@gmail.com>
2017-10-19 14:39:37 +0200

    Header: implementing initial structure

--- src/app/Header.js
+++ src/app/Header.js
@@ -4,7 +4,18 @@ import './Header.css';
 class Header extends React.Component {
   render() {
     return (
-      <header></header>
+      <header>
+        <a href="#">
+          <h1>TransferSmart</h1>
+        </a>
+
+        <ul>
+          <li><a href="#">About us</a></li>
+          <li><a href="#">Rates</a></li>
+          <li><a href="#">Login</a></li>
+          <li className="highlight"><a href="#">Sign Up</a></li>
+        </ul>
+      </header>
     );
   }
 }
