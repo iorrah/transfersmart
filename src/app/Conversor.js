8529309bba1a8a6ce7f2b8e242d45710418b439e
Iorrah Gonzalez <iorrah.gonzalez@gmail.com>
2017-10-24 21:03:51 +0200

    Removing code that is not in use

--- src/app/Conversor.js
+++ src/app/Conversor.js
@@ -4,14 +4,6 @@ import capitalize from '../utils/capitalize';
 import Mock from '../utils/Mock';
 import './Conversor.css';
 
-const API = {
-  host: 'https://txf-ecb.glitch.me',
-  path: 'rates',
-  url() {
-    return `${this.host}/${this.path}`;
-  },
-};
-
 class Conversor extends React.Component {
   constructor(props) {
     super(props);
@@ -35,60 +27,13 @@ class Conversor extends React.Component {
     this.onChange = this.onChange.bind(this);
   }
 
-  fetchData() {
-    /*
-      The API does not seem to be working
-      at present but this method was supposed
-      to fetch the needed data and set it to
-      our state properties.
-    */
-
-    // fetch(API.url())
-    //   .then((response) => { response.json(); })
-    //   .then(function (data) {
-    //     const rates = data.rates;
-    //     const from = { currency: data.base, rate: 0 };
-    //     const to = rates[0];
-    //     rates.push(to);
-
-    //     this.setState({ rates });
-    //     this.setState({ from });
-    //     this.setState({ to });
-    //   });
-
-    /*
-
-      Must handle any errors that hapen
-      in the process:
-
-      .catch(e) {
-        this.errors.push(e);
-      }
-
-    */
-
-    return fetch(API.url())
-      .then((response) => { response.json(); })
-      .then(function (data) {
-        return data;
-      });
-  }
-
   fetchMockData() {
-    let resolve = (data) => {
-      return data;
-    };
-
-    let reject = (desc) => {
-      console.error('Error: ' + desc);
-    };
-
     return new Promise((resolve, reject) => {
       setTimeout(function() {
         if (Mock.base) {
           return resolve(Mock);
         } else {
-          return reject('things went badly');
+          return reject();
         }
       }, 1000);
     });
