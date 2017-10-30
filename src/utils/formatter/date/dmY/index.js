8b1499adaad6746e8f1240612b0376f37467fe73
Iorrah Gonzalez <iorrah.gonzalez@gmail.com>
2017-10-30 16:52:56 +0100

    Removing code that is not in use

--- src/utils/formatter/date/dmY/index.js
+++ src/utils/formatter/date/dmY/index.js
@@ -3,16 +3,11 @@ import string from '../../string';
 const dmY = function (date) {
   const object = new Date(date);
 
-  const day = object.getDate();
-  const month = (object.getMonth() + 1);
-  const year = object.getFullYear();
-
-  // console.log('day: ' + day);
-  // console.log('month: ' + month);
-  // console.log('year: ' + year);
+  const day = object.getDate(),
+    month = (object.getMonth() + 1),
+    year = object.getFullYear();
 
   return `${string.pad(day)}/${string.pad(month)}/${year}`;
-  // return `${day}/${month}/${year}`;
 };
 
 export default dmY;
