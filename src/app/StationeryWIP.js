86d4f56db6088da049f50fd944cef2755806062d
Iorrah Gonzalez <iorrah.gonzalez@gmail.com>
2017-11-02 22:57:49 +0100

    Structural revisions

--- src/app/StationeryWIP.js
+++ src/app/StationeryWIP.js
@@ -1,37 +1,32 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import { FaCog, FaAngleLeft } from 'react-icons/lib/fa';
-
 import './StationeryWIP.css';
 
-class StationeryWIP extends React.Component {
-  onClick() {
-    window.history.back();
-  }
-
-  render() {
-    return (
-      <div className="stationery-wip">
-        <div className="mb-2 text-center">
-          <FaCog className="cog spin" />
-        </div>
-
-        <h2>
-          This section is under construction
-        </h2>
-
-        <h3>
-          Please, come back again later
-        </h3>
-
-        <div className="text-center">
-          <button className="btn btn-white btn-circular btn-rz-1 z-depth-1" title="Go back" onClick={this.onClick}>
-            <FaAngleLeft />
-          </button>
-        </div>
+const StationeryWIP = function () {
+  return (
+    <div className="stationery-wip">
+      <div className="mb-2 text-center">
+        <FaCog className="cog spin" />
       </div>
-    );
-  }
+
+      <h2>
+        This section is under construction
+      </h2>
+
+      <h3>
+        Please, come back again later
+      </h3>
+
+      <div className="text-center">
+        <Link to="/"
+          className="btn btn-white btn-circular btn-rz-1 z-depth-1"
+          title="Go back">
+          <FaAngleLeft />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default StationeryWIP;
