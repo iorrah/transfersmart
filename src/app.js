09666c7802fa740f8fbd383988c6ecb21bb15ebb
Iorrah Gonzalez <iorrah.gonzalez@gmail.com>
2017-11-02 22:40:50 +0100

    Updating links across the app

--- src/app.js
+++ src/app.js
@@ -8,9 +8,13 @@ import {
 import './app.css';
 import appSettings from './app/AppSettings';
 import Header from './app/Header';
+import Footer from './app/Footer';
 import Section from './app/Section';
 import AboutUs from './app/AboutUs';
-import Footer from './app/Footer';
+import FAQ from './app/FAQ';
+import Rates from './app/Rates';
+import LogIn from './app/LogIn';
+import SignUp from './app/SignUp';
 
 const App = () => (
   <Router basename={appSettings.base_name}>
@@ -18,6 +22,10 @@ const App = () => (
       <Header />
       <Route exact path="/" component={Section} />
       <Route path="/AboutUs" component={AboutUs} />
+      <Route path="/FAQ" component={FAQ} />
+      <Route path="/Rates" component={Rates} />
+      <Route path="/LogIn" component={LogIn} />
+      <Route path="/SignUp" component={SignUp} />
       <Footer />
     </div>
   </Router>
